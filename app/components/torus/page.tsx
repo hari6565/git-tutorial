f5f224623e31692af71dcb776dbc78bb271ae181
hari6565 <harin@torus.tech>
2024-02-27 11:38:17 +0530

    sidenavcomp

--- app/components/torus/page.tsx
+++ app/components/torus/page.tsx
@@ -1,13 +1,23 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import SideBar from "./sideBar";
 import FootBar from "./footer";
 import TopNavBar from "./TopNavBar";
+import SelectorSideBar from "../selectorSideBar/selectorSideBar";
 const page = () => {
+  const [state, setState] = useState("");
   return (
-    <div>
-      <TopNavBar />
-      <SideBar />
-      <FootBar />
+    <div className="h-sreen">
+      <div className="h-[10%]">
+        <TopNavBar />
+      </div>
+      <div className="h-[80%] flex ">
+        <SideBar state={state} setState={setState} />
+        <SelectorSideBar state={state} setState={setState} />
+      </div>
+      <div className="h-[10%]">
+        <FootBar />
+      </div>
     </div>
   );
 };
--- app/components/torus/sideBar.tsx
+++ app/components/torus/sideBar.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { Tabs } from "flowbite-react";
+import { ListGroup, Tooltip } from "flowbite-react";
 import React, { useState } from "react";
 import { FcStart } from "react-icons/fc";
 import { CgComponents } from "react-icons/cg";
@@ -7,167 +7,89 @@ import { IoDocumentsOutline } from "react-icons/io5";
 import { GrDocumentConfig } from "react-icons/gr";
 import TorusImg from "../../assests/torus.png";
 import Image from "next/image";
+import { LuListTree } from "react-icons/lu";
+import { FaRegNewspaper } from "react-icons/fa6";
+import { TbLayersLinked } from "react-icons/tb";
+import { FaTableList } from "react-icons/fa6";
+import { GrDatabase } from "react-icons/gr";
+import { TbFolderCog } from "react-icons/tb";
+import { TbApiApp } from "react-icons/tb";
+import { GrMultimedia } from "react-icons/gr";
+import { IoSettingsOutline } from "react-icons/io5";
 
 const Icons = [
   {
     id: 1,
-    icon: CgComponents,
-    title: "DF",
-    data: [
-      {
-        id: 21,
-        label: "Data Collection",
-        description: "Collect relevant data",
-      },
-      { id: 22, label: "Data Analysis", description: "Analyze gathered data" },
-      {
-        id: 23,
-        label: "Data Visualization",
-        description: "Create visualizations",
-      },
-      { id: 24, label: "Data Storage", description: "Manage data storage" },
-    ],
+    icon: LuListTree,
+    tooltip: "tree",
   },
   {
     id: 2,
-    icon: FcStart,
-    title: "PF",
-    data: [
-      // {
-      //   id: 21,
-      //   label: "Process Collection",
-      //   description: "Collect relevant data",
-      // },
-      // {
-      //   id: 22,
-      //   label: "Process Analysis",
-      //   description: "Analyze gathered data",
-      // },
-      // {
-      //   id: 23,
-      //   label: "Process Visualization",
-      //   description: "Create visualizations",
-      // },
-      // { id: 24, label: "Process Storage", description: "Manage data storage" },
-    ],
+    icon: FaRegNewspaper,
+    tooltip: "page selector",
   },
   {
     id: 3,
-    icon: IoDocumentsOutline,
-    title: "UF",
-    data: [
-      {
-        id: 31,
-        label: "Process Definition",
-        description: "Define project processes",
-      },
-      {
-        id: 32,
-        label: "Workflow Optimization",
-        description: "Optimize workflow",
-      },
-      {
-        id: 33,
-        label: "Process Documentation",
-        description: "Document project processes",
-      },
-      {
-        id: 34,
-        label: "Continuous Improvement",
-        description: "Implement continuous improvement",
-      },
-    ],
+    icon: TbLayersLinked,
+    tooltip: "storyboard",
   },
   {
     id: 4,
-    icon: TorusImg,
-    title: "TRN",
-    data: [
-      {
-        id: 41,
-        label: "Document Management",
-        description: "Manage project documents",
-      },
-      {
-        id: 42,
-        label: "Document Review",
-        description: "Review important documents",
-      },
-      {
-        id: 43,
-        label: "Version Control",
-        description: "Control document versions",
-      },
-      {
-        id: 44,
-        label: "Document Archive",
-        description: "Archive outdated documents",
-      },
-    ],
+    icon: FaTableList,
+    tooltip: "firestore",
   },
   {
     id: 5,
-    icon: GrDocumentConfig,
-    title: "System",
-    data: [
-      {
-        id: 51,
-        label: "System Configuration",
-        description: "Configure project system",
-      },
-      {
-        id: 52,
-        label: "Integration",
-        description: "Integrate system components",
-      },
-      {
-        id: 53,
-        label: "System Monitoring",
-        description: "Monitor system performance",
-      },
-      {
-        id: 54,
-        label: "System Maintenance",
-        description: "Perform system maintenance",
-      },
-    ],
+    icon: GrDatabase,
+    tooltip: "datatypes",
+  },
+  {
+    id: 6,
+    icon: TbFolderCog,
+    tooltip: "app values",
+  },
+  {
+    id: 7,
+    icon: TbApiApp,
+    tooltip: "api calls",
+  },
+  {
+    id: 8,
+    icon: GrMultimedia,
+    tooltip: "media",
+  },
+  {
+    id: 9,
+    icon: IoSettingsOutline,
+    tooltip: "settings",
   },
 ];
 
-export default function Component() {
-  const [state, setState] = useState("");
+export default function Component({ state, setState }) {
   return (
-    <div className="h-[88vh] w-[70px] bg-gray-200 flex flex-col justify-evenly ">
-      <Tabs
-        aria-label="Default tabs"
-        style="default"
-        className="flex flex-col w-[70px] gap-8"
-        onClick={(e: any) => setState(e?.target?.innerText)}
-      >
-        {Icons.map((ele: any) => {
-          return (
-            <Tabs.Item
-              key={ele.id}
-              title={
-                <div className={`flex flex-col items-center w-[35px]`}>
-                  {ele.icon == TorusImg ? (
-                    <Image src={TorusImg} alt="Torus" width={20} height={20} />
-                  ) : (
-                    React.createElement(ele.icon, { size: 20 })
-                  )}
-                  <div className="text-[10px]">{ele.title}</div>
-                </div>
+    <div className="h-[85vh] flex flex-col bg-slate-400 dark:bg-gray-800  w-[60px]">
+      <ListGroup className="bg-slate-400 dark:bg-gray-800 flex flex-col gap-4 rounded-none border-none mt-5 items-center w-[60px] justify-between h-[80%]">
+        {Icons.map((item) => (
+          // <div className="flex">
+          <Tooltip
+            content={item.tooltip}
+            style="light"
+            arrow={false}
+            placement="right"
+          >
+            <ListGroup.Item
+              className={
+                "flex flex-col px-0 dark:text-white w-[50px] bg-slate-400 py-0 rounded-lg"
               }
+              style={{ flexDirection: "column" }}
+              onClick={() => setState(item.tooltip)}
             >
-              <div className="absolute left-[150px] top-0 flex flex-col justify-evenly">
-                <span className="font-medium text-gray-800 dark:text-white">
-                  {/* next component here */}
-                </span>
-              </div>
-            </Tabs.Item>
-          );
-        })}
-      </Tabs>
+              <div>{React.createElement(item.icon)}</div>
+            </ListGroup.Item>
+          </Tooltip>
+          // </div>
+        ))}
+      </ListGroup>
     </div>
   );
 }
