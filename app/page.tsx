5220ff89ed0f19d1393385cbaf5863f121479c02
aathilaksh <lakshmiu@torus.tech>
2024-02-27 15:17:39 +0530

    add a file

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,19 @@
 import React from "react";
-import { Component } from "./components/MainPage";
-const page = () => {
-  return (
-    <div>
-      <Component />
-      
-    </div>
-  );
-};
+// import { Component } from "./components/MainPage";
+//  import Sidebar from './components/Sidebar/page'
+ import Dataflow from './components/Dataflow/page'
+ import Properties from '../app/components/Properties'
+ 
+const page = () => (
+  <div>
+    {/* <Component />  */}
+    {/* <Sidebar/> */}
+    {/* <Dataflow /> */}
+    <Properties/>
+    
+
+
+  </div>
+);
 
 export default page;
